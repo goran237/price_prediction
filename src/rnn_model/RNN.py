4cdf8c6fa50f1cf3ce64d62e654b4dec5d629ed2
Goran Kurucev <goran237@gmail.com>
2019-03-19 20:31:46 +0100

    finish build_graph

--- src/rnn_model/RNN.py
+++ src/rnn_model/RNN.py
@@ -40,5 +40,44 @@ class RNN(object):
             return lstm_cell
 
         cell = tf.nn.rnn_cell.MultiRNNCell([create_one_cell() for _ in range(self.num_layers)], state_is_tuple=True) if self.num_layers>1 else create_one_cell()
+        if self.embed_size >0 and self.stock_count>1:
+            self.embed_matrix = tf.Variable(tf.random_uniform([self.stock_count,self.embed_size],-1.0,1.0),name="embed_matrix")
+
+            stacked_symbols = tf.tile(self.symbols,[1,self.num_steps], name='stacked_stock_labels')
+            stacked_embeds = tf.nn.embedding_lookup(self.embed_matrix,stacked_symbols)
+
+            self.inputs_with_embeds = tf.concat([self.inputs,stacked_embeds], axis=2, name='inputs_wit_embed')
+            self.embed_matrix_summ = tf.summary.histogram('embed_matrix', self.embed_matrix)
+        else:
+            self.inputs_with_embeds = tf.identity(self.inputs)
+            self.embed_matrix_summ = None
+        print("inputs.shape:", self.inputs.shape)
+        print("inputs_with_embed.shape:", self.inputs_with_embed.shape)
+
+        val, state_ = tf.nn.dynamic_rnn(cell,self.inputs_with_embeds,dtype=tf.float32, scope = 'dynamix_rnn')
+        val = tf.transpose(val,[1,0,2])
+
+        last = tf.gather(val, int(val.get_shape()[0])-1, name='lstm_state')
+        ws = tf.Variable(tf.truncated_normal([self.lstm_size,self.input_size]), name='w')
+        bias = tf.Variable(tf.constant(0.1,shape=[self.input_size]), name='b')
+        self.pred = tf.matmul(last,ws)+bias
+
+        self.last_sum = tf.summary.histogram('lstm_state', last)
+        self.w_sum = tf.summary.histogram('w',ws)
+        self.b_sum = tf.summary.histogram('b',bias)
+        self.pred_summ = tf.summary.histogram('pred', self.pred)
+
+        self.loss = tf.reduce_mean(tf.square(self.pred - self.targets), name='loss_mse_train')
+        self.optim = tf.train.RMSPropOptimizer(self.learning_rate).minimize(self.loss, name='rmsprop_optim')
+
+        self.loss_test = tf.reduce_mean(tf.square(self.pred - self.targets), name='loss_mse_test')
+
+        self.loss_sum = tf.summary.scalar('loss_mse_train', self.loss)
+        self.loss_test_sum = tf.summary.scalar('loss_mse_test', self.loss_test)
+        self.learning_rate_sum = tf.summary.scalar('learning_rate', self.learning_rate)
+
+        self.t_vars = tf.trainable_variables()
+        self.saver = tf.train.Saver()
+
 
 
